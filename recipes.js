bee1e81e05581569214081d3c3f5e0875af777ad
yoann <yoann.lal@gmail.com>
2024-06-27 10:16:08 +0200

    server.js

---
+++ recipes.js
@@ -0,0 +1,60 @@
+export const recipes = [
+  {
+    uuid: 1,
+    name: "Baked Salmon",
+    ingredients: [
+      { name: "Salmon", amount: 1, measurement: "l lb" },
+      { name: "Pine Nuts", amount: 1, measurement: "cup" },
+      { name: "Butter Lettuce", amount: 2, measurement: "cups" },
+      { name: "Yellow Squash", amount: 1, measurement: "med" },
+      { name: "Olive Oil", amount: 0.5, measurement: "cup" },
+      { name: "Garlic", amount: 3, measurement: "cloves" },
+    ],
+    steps: [
+      "Preheat the oven to 350 degrees.",
+      "Spread the olive oil around a glass baking dish.",
+      "Add the yellow squash and place in the oven for 30 mins.",
+      "Add the salmon, garlic, and pine nuts to the dish.",
+      "Bake for 15 minutes.",
+      "Remove from oven. Add the lettuce and serve.",
+    ],
+    rating: 4,
+  },
+  {
+    uuid: 2,
+    name: "Fish Tacos",
+    ingredients: [
+      { name: "Whitefish", amount: 1, measurement: "l lb" },
+      { name: "Cheese", amount: 1, measurement: "cup" },
+      { name: "Iceberg Lettuce", amount: 2, measurement: "cups" },
+      { name: "Tomatoes", amount: 2, measurement: "large" },
+      { name: "Tortillas", amount: 3, measurement: "med" },
+      { name: "Corn", amount: 1, measurement: "can" },
+    ],
+    steps: [
+      "Cook the fish on the grill until cooked through.",
+      "Place the fish on the 3 tortillas.",
+      "Top them with lettuce, tomatoes, corn and cheese.",
+    ],
+    rating: 2,
+  },
+  {
+    uuid: 3,
+    name: "Chicken Noodle Soup",
+    ingredients: [
+      { name: "Extra virgin olive oil", amount: 3, measurement: "tablespoons" },
+      { name: "Onion", amount: 1, measurement: "yellow" },
+      { name: "Carots", amount: 2, measurement: "medium" },
+      { name: "Celery", amount: 2, measurement: "stalks" },
+      { name: "Egg noodles", amount: 1, measurement: "package" },
+      { name: "Cooked chicken", amount: 1, measurement: "cup" },
+    ],
+    steps: [
+      "Chop and saute onion, carrots and celery in olive oil until soft.",
+      "Add chicken broth and bring to boil",
+      "Add egg noodles and cook until soft",
+      "Add chicken",
+    ],
+    rating: 5,
+  },
+];
---
+++ server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import { recipes } from "./recipes.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 5000;
+
+let recipesJson = recipes;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World 2!");
+});
+
+app.get("/test", (req, res) => {
+  res.send("test");
+});
+
+app.get("/recipes", (req, res) => {
+  res.json({ data: recipesJson, status: 200 });
+});
+
+app.post("/recipes", (req, res) => {
+  const newRecipe = {
+    ...req.body,
+    uuid: recipesJson[recipesJson.length - 1].uuid,
+  };
+
+  recipesJson.push(newRecipe);
+
+  res.json({ data: newRecipe, status: 200 });
+});
+
+// Edit recipe
+// app.put("/recipes/:recipeId", (req, res) => {
+//   const recipesJson = recipes;
+
+//   res.json({ data: recipesJson, status: 200 });
+// });
+
+// Delete recipe
+// app.delete("/recipes/:recipeId", (req, res) => {
+//   const recipesJson = recipes;
+
+//   res.json({ data: recipesJson, status: 200 });
+// });
+
+//
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
